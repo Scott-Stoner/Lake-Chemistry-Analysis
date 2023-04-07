5aa29076f936a4c4b8720f31d47e09f17d7b3e15
Scott Stoner <129141312+Scott-Stoner@users.noreply.github.com>
2023-04-07 03:26:05 -0700

    pca and rf

--- scripts/Testpad.R
+++ scripts/Testpad.R
@@ -4,5 +4,16 @@ phyto_rf[,-c(1)] %>%
 
 test_pca
 summary(test_pca) -> pca_summary
-
 pca_summary
+biplot(test_pca)
+test_pca$x %>%
+  data.frame() %>%
+  ggplot(aes(x = PC1, y = PC2, color = phyto_rf$PTL_RESULT)) +
+  geom_point()
+
+
+
+phyto_filtered %>% group_by(TAXA_ID) %>% count(TAXA_ID) -> test_guy
+
+
+tb_histogram_panel(watershed_filter_result, PTL_RESULT, .cols)
